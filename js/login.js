a606dd429e57cbbfae07a855cbe48f9962aa06c2
Aurora-YS <yonddoni@gmail.com>
2021-09-27 21:35:18 +0900

    3333333 commit

--- js/login.js
+++ js/login.js
@@ -2,5 +2,26 @@ $(document).ready(function(){
     
     /* login.js */
 
+    $("#section_01 #signin_form .txt_box #signin_id").blur(function(){
+        let $id_val = $(this).val();
+        if($id_val.trim() == ""){
+            $(this).siblings("p").show();
+            return false;
+        }else{
+            $(this).siblings("p").hide();
+        }
+    });
+
+
+    $("#section_01 #signin_form .txt_box #signin_pw").blur(function(){
+        let $pw_val = $(this).val();
+        if($pw_val.trim() == ""){
+            $(this).siblings("p").show();
+            return false;
+        }else{
+            $(this).siblings("p").hide();
+        }
+    });
+
 
 });
